9ac7eafc3c0db64d6aa83714719f3bcceffb8e7f
Mohsen Mirshahreza <mirshahreza@outlook.com>
2024-04-02 15:22:00 +0330

    About AppEnd updated

--- AppEndHost/workspace/client/.PublicComponents/baseAbout.vue
+++ AppEndHost/workspace/client/.PublicComponents/baseAbout.vue
@@ -1,14 +1,12 @@
 <template>
-    <div class="col-48 h-100 p-2">
+    <div class="col-48 h-100 p-3">
 
-        <div class="text-center mb-4">
-            <img src="/..lib/images/AppEnd-Logo-Full.png" class="border border-2 rounded rounded-2 shadow-sm w-100" />
+        <div class="text-center my-4 mb-5">
+            <img src="/..lib/images/AppEnd-Logo-Full.png" class="border border-2 rounded rounded-2 shadow-sm" style="width:40%" />
         </div>
         <div class="px-2" style="direction:ltr;text-align:left">
-            <div class="fs-d9 fb text-truncate" style="user-select:none;">
-                <span style="color:#d50000;" class="app-title">AppEnd</span> <span style="color:#9E9E9E;" class="app-sub-title">Studio</span>
-            </div>
-            <div class="fs-d8">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;AppEnd Studio is a fullstack low-code environment. It helps you to create APIs, user interfaces and manage access levels.</div>
+            <div class="fs-d8"><span class="fw-bold">AppEnd</span> is a fullstack low-code environment. It helps you to create APIs, user interfaces and manage access levels.</div>
+            <div class="my-1 fw-bold fs-d7 text-muted">To learn more about AppEnd go to <a href="https://github.com/mirshahreza/AppEnd">GitHub Repository</a></div>
             <div class="fs-d9 fb text-muted mt-4">Version 2.0</div>
         </div>
     </div>
