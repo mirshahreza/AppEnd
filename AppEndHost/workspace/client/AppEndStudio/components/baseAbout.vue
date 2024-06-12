3adcf3d1cf9126d355216d8dad24f8e8463330d0
Mohsen Mirshahreza <mirshahreza@outlook.com>
2024-06-12 19:13:41 +0330

    Template bugfix UI Optimization

--- AppEndHost/workspace/client/AppEndStudio/components/baseAbout.vue
+++ AppEndHost/workspace/client/AppEndStudio/components/baseAbout.vue
@@ -3,7 +3,7 @@
         <div class="row">
             <div class="col-48 h-100 p-3">
                 <div class="text-center my-4 mb-5">
-                    <img src="/a..lib/images/AppEnd-Logo-Full.png" class="border border-2 rounded rounded-2 shadow-sm" style="width:40%" />
+                    <img src="/a..lib/images/Logo-Full.png" class="border border-2 rounded rounded-2 shadow-sm" style="width:40%" />
                 </div>
                 <div class="px-2" style="direction:ltr;text-align:left">
                     <div class="fs-d8"><span class="fw-bold">AppEnd</span> is a fullstack low-code environment. It helps you to create APIs, user interfaces and manage access levels.</div>
