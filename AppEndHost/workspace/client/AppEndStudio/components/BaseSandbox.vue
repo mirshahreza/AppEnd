5a61a77ab501e0852d5265fd51146ad1f63f2b75
Mohsen Mirshahreza <mirshahreza@outlook.com>
2024-08-30 06:54:10 +0330

    some bg examples Added

--- AppEndHost/workspace/client/AppEndStudio/components/BaseSandbox.vue
+++ AppEndHost/workspace/client/AppEndStudio/components/BaseSandbox.vue
@@ -1,5 +1,88 @@
 <template>
     <div class="container-fluid h-100 p-5 scrollable">
+
+        <div class="card my-2">
+            <div class="card-body text-center">
+
+                <div class="p-3 mb-2 bg-primary text-white">.bg-primary</div>
+
+                <div class="p-3 mb-2 bg-primary-subtle text-primary-emphasis">.bg-primary-subtle</div>
+                <div class="p-3 mb-2 bg-primary-subtle text-primary-emphasis bg-gradient">.bg-primary-subtle .bg-gradient</div>
+
+                <div class="p-3 mb-2 bg-secondary text-white">.bg-secondary</div>
+                <div class="p-3 mb-2 bg-secondary-subtle text-secondary-emphasis">.bg-secondary-subtle</div>
+
+                <div class="row">
+                    <div class="col">
+                        <div class="p-3 mb-2 bg-success text-white">
+                            .bg-success
+                            <br />
+                            .bg-success
+                            <br />
+                            .bg-success
+                            <br />
+                            .bg-success
+                            <br />
+                            .bg-success
+                            <br />
+                            .bg-success
+                            <br />
+                            .bg-success
+                            <br />
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="p-3 mb-2 bg-success text-white bg-gradient">
+                            .bg-success .bg-gradient
+                            <br />
+                            .bg-success .bg-gradient
+                            <br />
+                            .bg-success .bg-gradient
+                            <br />
+                            .bg-success .bg-gradient
+                            <br />
+                            .bg-success .bg-gradient
+                            <br />
+                            .bg-success .bg-gradient
+                            <br />
+                            .bg-success .bg-gradient
+                            <br />
+                        </div>
+                    </div>
+                </div>
+
+                <div class="p-3 mb-2 bg-success-subtle text-success-emphasis">.bg-success-subtle</div>
+                <div class="p-3 mb-2 bg-danger text-white">.bg-danger</div>
+                <div class="p-3 mb-2 bg-danger-subtle text-danger-emphasis">.bg-danger-subtle</div>
+                <div class="p-3 mb-2 bg-warning text-dark">.bg-warning</div>
+                <div class="p-3 mb-2 bg-warning-subtle text-warning-emphasis">.bg-warning-subtle</div>
+
+                <div class="row">
+                    <div class="col">
+                        <div class="p-3 mb-2 bg-info text-dark">.bg-info</div>
+                    </div>
+                    <div class="col">
+                        <div class="p-3 mb-2 bg-info text-dark bg-gradient">.bg-info .bg-gradient</div>
+                    </div>
+                </div>
+
+                <div class="p-3 mb-2 bg-info-subtle text-info-emphasis">.bg-info-subtle</div>
+                <div class="p-3 mb-2 bg-light text-dark">.bg-light</div>
+                <div class="p-3 mb-2 bg-light-subtle text-light-emphasis">.bg-light-subtle</div>
+                <div class="p-3 mb-2 bg-dark text-white">.bg-dark</div>
+                <div class="p-3 mb-2 bg-dark-subtle text-dark-emphasis">.bg-dark-subtle</div>
+                <div class="p-3 mb-2 bg-body-secondary">.bg-body-secondary</div>
+                <div class="p-3 mb-2 bg-body-tertiary">.bg-body-tertiary</div>
+                <div class="p-3 mb-2 bg-body text-body">.bg-body</div>
+                <div class="p-3 mb-2 bg-black text-white">.bg-black</div>
+                <div class="p-3 mb-2 bg-white text-dark">.bg-white</div>
+                <div class="p-3 mb-2 bg-transparent text-body">.bg-transparent</div>
+
+
+            </div>
+        </div>
+
+
         <div class="card my-2">
             <div class="card-body text-center">
                 <i class="fa-solid fa-fw fa-a text-danger"></i>
