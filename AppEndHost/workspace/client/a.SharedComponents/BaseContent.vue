dede7628594b1d60ab1a3cda7858b11e50eaec27
Mohsen Mirshahreza <mirshahreza@outlook.com>
2024-09-07 11:03:49 +0330

    BaseContent added

---
+++ AppEndHost/workspace/client/a.SharedComponents/BaseContent.vue
@@ -0,0 +1,34 @@
+﻿<template>
+    <div class="card border-0 h-100">
+        <div class="card-header py-3" v-if="shared.fixNull(content.Title,'')!==''">
+            <div class="fs-1d2 fw-bold text-primary">
+                {{content.Title}}
+            </div>
+        </div>
+        <div class="card-header py-1 bg-success-subtle" v-if="shared.fixNull(content.CreatedOn,'')!==''">
+            <div class="fw-bold text-danger fs-d8">
+                {{shared.formatDate(content.CreatedOn)}}
+            </div>
+        </div>
+        <div class="card-body p-5 pt-4">
+            <div v-html="content.ContentBody"></div>
+        </div>
+    </div>
+</template>
+
+<script>
+    let _this = { cid: "", c: null, content: { Title: "", ContentBody: "" } };
+    export default {
+        methods: {
+        },
+        setup(props) {
+            _this.cid = props['cid'];
+            _this.inputs = shared["params_" + _this.cid];
+            _this.content = fixNull(_this.inputs["content"], '');
+        },
+        data() { return _this; },
+        created() { _this.c = this; },
+        mounted() { },
+        props: { cid: String }
+    }
+</script>
