10b4fd6a1b1d7ea057f72d404e1e9af411f6d1af
Mohsen Mirshahreza <m_mirshahreza@yahoo.com>
2025-11-29 16:52:39 +0330

    Refactor layout and simplify components
    Refactored `BaseHome.vue` to reduce nested card structures, reorganize layout, and improve maintainability. Moved `DigitalClock` and `MySummary` components into separate cards with updated styles. Restructured "Actions" section into its own card and relocated `MyShortcuts`, `BaseServerSummary`, and `BaseSubApps` components to a new column for better organization.
    
    Simplified `DigitalClock.vue` by removing unnecessary nesting and reducing the clock display to a single line of HTML. These changes enhance readability, maintainability, and performance.

--- AppEndHost/workspace/client/AppEndStudio/components/BaseHome.vue
+++ AppEndHost/workspace/client/AppEndStudio/components/BaseHome.vue
@@ -1,40 +1,44 @@
 <template>
     <div class="card h-100 rounded-bottom-0 rounded-end-0 bg-transparent border-0">
-        <div class="card-body p-2">
-            <div class="card h-100 border-light bg-light bg-opacity-75 border-0">
-                <div class="card-body rounded rounded-2 border border-3 border-light fs-d8 p-3 bg-transparent">
-                    <div class="container-fluid">
-                        <div class="row h-100 bg-transparent">
-                            <div class="col-48 col-md-10">
-                                <div class="card" style="background-color:#fff7f7">
-                                    <div class="card-body">
-                                        <component-loader src="/a.SharedComponents/DigitalClock" uid="digitalClock" />
-                                        <component-loader src="/a.SharedComponents/MySummary" uid="mySummary" />
-                                    </div>
+        <div class="card-body p-2 p-md-5">
+            <div class="container-fluid">
+                <div class="row h-100 bg-transparent">
+                    <div class="col-48 col-md-6">
+                        
+                        <div class="card" style="background-color:#f4ffe9">
+                            <div class="card-body">
+                                <div class="font-monospace text-center fs-1d5">
+                                    <component-loader src="/a.SharedComponents/DigitalClock" uid="digitalClock" />
                                 </div>
-                                <div class="card mt-2">
-                                    <div class="card-body">
-                                        Actions
-                                        <hr class="my-1 border-3 border-secondary-subtle" />
-                                        <div class="btn btn-sm btn-link text-decoration-none ps-1" @click="reBuild">
-                                            <i class="fa-solid fa-fw fa-chevron-right"></i> <span>ReBuild Code Files</span>
-                                        </div>
-                                        <br />
-                                        <div class="btn btn-sm btn-link text-decoration-none ps-1" @click="refreshSession">
-                                            <i class="fa-solid fa-fw fa-chevron-right"></i> <span>Refresh Session</span>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-48 col-md-2"></div>
-                            <div class="col-48 col-md-36">
-                                <component-loader src="/a.SharedComponents/MyShortcuts" uid="myShortcuts" />
-                                <div class="p-2">&nbsp;</div>
-                                <component-loader src="components/BaseServerSummary" uid="baseServerSummary" />
-                                <div class="p-2">&nbsp;</div>
-                                <component-loader src="/a.SharedComponents/BaseSubApps" uid="baseSubApps" />
                             </div>
                         </div>
+
+                        <div class="card mt-2" style="background-color:#fff7f7">
+                            <div class="card-body">
+                                <div class="font-monospace text-center fs-1d3">
+                                    <component-loader src="/a.SharedComponents/MySummary" uid="mySummary" />
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="card mt-2">
+                            <div class="card-body">
+                                Actions
+                                <hr class="my-1 border-3 border-secondary-subtle" />
+                                <div class="btn btn-sm btn-link text-decoration-none ps-1" @click="reBuild">
+                                    <i class="fa-solid fa-fw fa-chevron-right"></i> <span>ReBuild Code Files</span>
+                                </div>
+                                <br />
+                                <div class="btn btn-sm btn-link text-decoration-none ps-1" @click="refreshSession">
+                                    <i class="fa-solid fa-fw fa-chevron-right"></i> <span>Refresh Session</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-48 col-md-10">
+                        <component-loader src="/a.SharedComponents/MyShortcuts" uid="myShortcuts" />
+                        <component-loader src="components/BaseServerSummary" uid="baseServerSummary" />
+                        <component-loader src="/a.SharedComponents/BaseSubApps" uid="baseSubApps" />
                     </div>
                 </div>
             </div>
--- AppEndHost/workspace/client/a.SharedComponents/DigitalClock.vue
+++ AppEndHost/workspace/client/a.SharedComponents/DigitalClock.vue
@@ -1,19 +1,5 @@
 <template>
-    <div class="card bg-transparent rounded-0 border-0">
-        <div class="card-body p-1">
-            <div class="card border-light bg-transparent bg-opacity-75 border-0">
-                <div class="card-body rounded rounded-1 border border-3 border-light fs-d9 p-2 bg-transparent">
-                    <div class="container-fluid">
-                        <div class="row fs-1d5">
-                            <div class="col-48 text-center fs-1d5 font-monospace">
-                                <span class="fw-bold text-primary timeH">00</span>:<span class="fw-bold text-success timeM">00</span>:<span class="text-secondary timeS">00</span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+    <span class="fw-bold text-primary timeH">00</span>:<span class="fw-bold text-success timeM">00</span>:<span class="text-secondary timeS">00</span>
 </template>
 <script>
     let _this = { cid: "", c: null, alloweds: [] };
