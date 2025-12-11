8a67c4d01a05f05e8ec63f09cc141123a164c109
Mohsen Mirshahreza <m_mirshahreza@yahoo.com>
2025-12-11 18:16:27 +0330

    Optimize reflection with caching; switch to System.Text.Json
    - Added thread-safe caching for type/method lookups in ExtensionsForTypeAndAssembly.cs to improve performance.
    - Replaced Newtonsoft.Json with System.Text.Json for RPC responses and set Content-Type to "application/json".
    - Updated ToDo.txt with a note to add rate limiting to method options.

--- AppEndCommon/ExtensionsForTypeAndAssembly.cs
+++ AppEndCommon/ExtensionsForTypeAndAssembly.cs
@@ -1,16 +1,28 @@
 ﻿using System.Reflection;
+using System.Collections.Concurrent;
 
 namespace AppEndCommon
 {
 	public static class ExtensionsForTypeAndAssembly
     {
+        private static readonly ConcurrentDictionary<string, Type[]> _typesCache = new();
+        private static readonly ConcurrentDictionary<string, MethodInfo[]> _methodsCache = new();
+
         public static Type[] GetTypesReal(this Assembly asm)
         {
-            return asm.GetTypes().Where(i => !i.Name.ContainsIgnoreCase("EmbeddedAttribute") && !i.Name.ContainsIgnoreCase("RefSafetyRulesAttribute")).ToArray();
+            string key = asm.FullName ?? asm.GetName().Name ?? "unknown";
+            if (_typesCache.TryGetValue(key, out var cached)) return cached;
+            var types = asm.GetTypes().Where(i => !i.Name.ContainsIgnoreCase("EmbeddedAttribute") && !i.Name.ContainsIgnoreCase("RefSafetyRulesAttribute")).ToArray();
+            _typesCache[key] = types;
+            return types;
         }
         public static MethodInfo[] GetMethodsReal(this Type type)
         {
-            return type.GetMethods().Where(m => !m.Name.Equals("GetType") && !m.Name.Equals("ToString") && !m.Name.Equals("Equals") && !m.Name.Equals("GetHashCode")).ToArray();
+            string key = type.FullName ?? type.Name;
+            if (_methodsCache.TryGetValue(key, out var cached)) return cached;
+            var methods = type.GetMethods().Where(m => !m.Name.Equals("GetType") && !m.Name.Equals("ToString") && !m.Name.Equals("Equals") && !m.Name.Equals("GetHashCode")).ToArray();
+            _methodsCache[key] = methods;
+            return methods;
         }
     }
 }
