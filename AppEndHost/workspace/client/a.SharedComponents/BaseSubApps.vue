56aec1f7523bdf5e50dc80d98c82e7a5d14bff80
Mohsen Mirshahreza <m_mirshahreza@yahoo.com>
2026-01-02 16:03:02 +0330

    Improve app link button style and text layout
    Updated app link buttons with border-0 and bg-light-subtle classes for a lighter look. Simplified display text to show app name and details in a single line, while keeping subtitle and title below.

--- AppEndHost/workspace/client/a.SharedComponents/BaseSubApps.vue
+++ AppEndHost/workspace/client/a.SharedComponents/BaseSubApps.vue
@@ -2,15 +2,11 @@
     <div class="card shadow-sm mb-2">
         <div class="card-body">
             <div v-for="app in apps">
-                <a :href="'/'+app.Name+'/'" target="_blank" class="btn btn-sm btn-outline-primary rounded-3 w-100 my-2 text-decoration-none pointer text-start">
+                <a :href="'/'+app.Name+'/'" target="_blank" class="btn btn-sm btn-outline-primary rounded-3 border-0 bg-light-subtle w-100 my-2 text-decoration-none pointer text-start">
                     <i class="fa-solid fa-fw fa-play"></i>
-
-                    <span>Folder : <span class="fw-bold">{{app.Name}}</span></span>
-
                     <span class="">
-                        [ {{app.Value.lang}} / {{app.Value.dir}} / {{app.Value.calendar}} ]
+                        {{app.Name}} :: {{app.Value.lang}} / {{app.Value.dir}} / {{app.Value.calendar}}
                     </span>
-
                     <span class="d-block fw-normal ps-3">
                         {{app.Value.title}} {{app.Value['sub-title']}}
                     </span>
