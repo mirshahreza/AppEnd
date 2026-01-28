028a6a7efabe920b90699464c451d4a8db14d37a
Mohsen Mirshahreza <m_mirshahreza@yahoo.com>
2026-01-28 07:26:57 +0330

    Ace Editor: switch to dynamic loading, add CSS/JSON workers
    Removed static Ace mode/theme/worker files and updated project configuration to use Ace Editor's dynamic module loader for modes, themes, and workers. Integrated new CSS and JSON workers for real-time linting and syntax validation. This streamlines Ace integration, reduces output size, and improves editor functionality.

--- AppEndHost/workspace/client/a..lib/append/js/append-jQuery-plugins.js
+++ AppEndHost/workspace/client/a..lib/append/js/append-jQuery-plugins.js
@@ -535,6 +535,13 @@
             let retTo = _this.parent().find("input");
             options = _.defaults(options, { mode: "ace/mode/csharp" });
             options["value"] = retTo.val() === null || retTo.val() === undefined || retTo.val() === '' ? '' : retTo.val();
+            
+            // Configure Ace Editor to load additional files from the correct path
+            ace.config.set('basePath', '/a..lib/ace/src-min');
+            ace.config.set('modePath', '/a..lib/ace/src-min');
+            ace.config.set('themePath', '/a..lib/ace/src-min');
+            ace.config.set('workerPath', '/a..lib/ace/src-min');
+            
             shared.editors[_this.attr("id")] = ace.edit(_this.attr("id"), options);
             if (retTo.attr("disabled")) shared.editors[_this.attr("id")].setReadOnly(true);
             shared.editors[_this.attr("id")].getSession().on('change', function () {
