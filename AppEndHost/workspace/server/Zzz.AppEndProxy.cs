fccd09bcdb07ee16d5904235f093eb2b1781a527
erfankarimi287 <erfankarimi287@gmail.com>
2026-02-13 18:13:07 +0330

    Add System.Threading namespace to Zzz.AppEndProxy.cs
    This update includes the addition of the System.Threading namespace to the Zzz.AppEndProxy.cs file, which may be necessary for implementing asynchronous operations or managing threads in future enhancements.

--- AppEndHost/workspace/server/Zzz.AppEndProxy.cs
+++ AppEndHost/workspace/server/Zzz.AppEndProxy.cs
@@ -14,6 +14,7 @@ using System.IO;
 using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Reflection;
 using System.Text;
