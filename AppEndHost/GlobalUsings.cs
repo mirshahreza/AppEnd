830ad9ea1af1a164ec1f196675da4f7fcdec8e59
Mohsen Mirshahreza <m_mirshahreza@yahoo.com>
2026-02-14 16:47:56 +0330

    Some Usings added to global usings file

--- AppEndHost/GlobalUsings.cs
+++ AppEndHost/GlobalUsings.cs
@@ -1,12 +1,16 @@
 global using System;
 global using System.Collections;
 global using System.Collections.Generic;
+global using System.Security.Cryptography;
 global using System.Data;
 global using System.IO;
 global using System.IO.Compression;
 global using System.Linq;
 global using System.Net;
+global using System.Net.Http;
+global using System.Net.Http.Headers;
 global using System.Text.Json;
+global using System.Threading.Tasks;
 global using static System.Net.WebRequestMethods;
 global using System.Reflection;
 global using System.Text;
@@ -23,6 +27,7 @@ global using Microsoft.AspNetCore.StaticFiles;
 global using Microsoft.Extensions.DependencyInjection;
 global using Microsoft.Extensions.Hosting;
 global using Microsoft.AspNetCore.Routing.Matching;
+global using Microsoft.Identity.Client;
 
 global using AppEndCommon;
 global using AppEndDbIO;
@@ -33,4 +38,7 @@ global using AngleSharp.Common;
 global using AngleSharp.Text;
 
 global using Newtonsoft.Json.Linq;
+
+
+
 
